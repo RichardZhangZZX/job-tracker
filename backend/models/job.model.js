e9f83aa6e4c05aae60e3f710c5662d1696ed729a
Richard Zhang <richardzhangzzx@gmail.com>
2025-08-22 03:01:46 -0700

    Initial commit

---
+++ backend/models/job.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const jobSchema = new mongoose.Schema({
+    company:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String
+    },
+    date_posted:{
+        type: String
+    },
+    date_applied:{
+        type: String
+    },
+    status:{
+        type: String
+    },
+    link:{
+        type: String
+    },
+    note:{
+        type: String
+    },
+}, {
+    timestamps: true // createdAt, updatedAt
+});
+
+const Job = mongoose.model('Job', jobSchema);
+// Jobs
+
+export default Job;   
+
+
+
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js"; 
+
+import jobRoutes from "./routes/job.route.js";
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.json()); // allow us to accept JSON data in the req.body
+ 
+app.use("/api/jobs", jobRoutes)
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log('Server started at http://localhost:' + PORT);
+});
+ 
